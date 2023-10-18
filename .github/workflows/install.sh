7c4566bd89fadc724c0adf06ba915b6bce729bdc
Darius Neatu <dariusn@adobe.com>
2023-10-18 07:14:46 +0300

    actionus

--- .github/workflows/install.sh
+++ .github/workflows/install.sh
@@ -1,6 +1,7 @@
 set -e
 set -x
 
-sudo apt-get install python git
+sudo apt-get install python3 python-is-python
+sudo apt-get install git
 
 git submodule add "https://github.com/Sarcasm/run-clang-format.git"
